35ef4923d1eb501a818e550c02a5bf61c38f7ebb
Bharat Poptwani <bharat.p@goodworker.in>
2021-11-28 20:35:15 +0530

    react state management demonstration

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React,  { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
@@ -5,14 +6,20 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   const { title, amount, date } = props;
+  const [itemTitle, setTitle] = useState(title);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{itemTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>change title</button>
   </Card>
   )
 }
