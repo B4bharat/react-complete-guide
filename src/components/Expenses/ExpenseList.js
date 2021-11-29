dd926ea9cef1acc8ad4da2c78b87989906d229a9
Bharat Poptwani <bharat.p@goodworker.in>
2021-11-29 21:08:01 +0530

    ExpenseFilter component added

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,17 +1,28 @@
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 import './Expenses.css';
 
 function ExpenseList(props) {
   const { expenses } = props;
+  const [filterYear, setFilterYear] = useState(2022);
+
+  const changeYear = (year) => {
+    setFilterYear(year);
+  }
+
 
   return (
-    <Card className="expenses">
-      {expenses.map(expense => (
-        <ExpenseItem key={expense.id} {...expense} />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter onChangeYear={changeYear} />
+      <Card className="expenses">
+        {expenses.map(expense => (
+          <ExpenseItem key={expense.id} {...expense} />
+        ))}
+      </Card>
+    </div>
   );
 }
 
