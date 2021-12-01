2ba3d9f5212154c4a9cd74d6d622f24e2a1af976
Bharat Poptwani <bharat.p@goodworker.in>
2021-12-01 23:00:23 +0530

    resolved a minor bug

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -11,54 +11,75 @@ function ExpenseForm(props) {
 
   const titleChangeHandler = (evt) => {
     setTitle(evt.target.value);
-  }
+  };
 
   const amountChangeHandler = (evt) => {
     setAmount(evt.target.value);
-  }
+  };
 
   const dateChangeHandler = (evt) => {
     setDate(evt.target.value);
-  }
+  };
 
   const submitHandler = (evt) => {
     evt.preventDefault();
 
     const expenseData = {
       title,
-      amount,
+      amount: +amount,
       date: new Date(date),
-    }
+    };
 
     props.onSaveExpenseData(expenseData);
 
     setTitle('');
     setAmount('');
     setDate('');
-  }
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label htmlFor="expense-title">Title</label>
-          <input type="text" id="expense-title" value={title} onChange={titleChangeHandler} />
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label htmlFor='expense-title'>Title</label>
+          <input
+            type='text'
+            id='expense-title'
+            value={title}
+            onChange={titleChangeHandler}
+          />
         </div>
-        <div className="new-expense__control">
-          <label htmlFor="expense-amount">Amount</label>
-          <input type="number" id="expense-amount" value={amount} min="0.01" step="0.01" onChange={amountChangeHandler}  />
+        <div className='new-expense__control'>
+          <label htmlFor='expense-amount'>Amount</label>
+          <input
+            type='number'
+            id='expense-amount'
+            value={amount}
+            min='0.01'
+            step='0.01'
+            onChange={amountChangeHandler}
+          />
         </div>
-        <div className="new-expense__control">
-          <label htmlFor="expense-date">Date</label>
-          <input type="date" id="expense-date" value={date} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+        <div className='new-expense__control'>
+          <label htmlFor='expense-date'>Date</label>
+          <input
+            type='date'
+            id='expense-date'
+            value={date}
+            min='2019-01-01'
+            max='2022-12-31'
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
-      <div className="new-expense__actions">
-        <button type="button" onClick={onCancel}>Cancel</button>
-        <button type="submit">Add Expense</button>
+      <div className='new-expense__actions'>
+        <button type='button' onClick={onCancel}>
+          Cancel
+        </button>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
-  )
+  );
 }
 
 export default ExpenseForm;
